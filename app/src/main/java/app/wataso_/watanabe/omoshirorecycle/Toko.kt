f26a4dbb8bc7065d12ff4e7b38e389f60d15366c
lifeistech <2wata2wata2@gmail.com>
2021-05-27 01:08:40 +0900

    \initial commit

---
+++ app/src/main/java/app/wataso_/watanabe/omoshirorecycle/Toko.kt
@@ -0,0 +1,11 @@
+package app.wataso_.watanabe.omoshirorecycle
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Toko : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_toko)
+    }
+}
