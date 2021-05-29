45a35bc03a0593b2dbdcd0970458548835f1f1e2
lifeistech <2wata2wata2@gmail.com>
2021-05-29 11:18:47 +0900

    "仮commit"

--- app/src/main/java/app/wataso_/watanabe/omoshirorecycle/MainActivity.kt
+++ app/src/main/java/app/wataso_/watanabe/omoshirorecycle/MainActivity.kt
@@ -3,15 +3,59 @@ package app.wataso_.watanabe.omoshirorecycle
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import io.realm.Realm
+import io.realm.RealmResults
+import io.realm.Sort
 import kotlinx.android.synthetic.main.activity_main.*
+import java.net.URI.create
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
-
-
+    //realmの定義
+    private val realm: Realm by lazy {
+        Realm.getDefaultInstance()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val dataList = readAll()
+
+        // タスクリストが空だったときにダミーデータを生成する
+        if (dataList.isEmpty()) {
+            createDummyData()
+        }
+
+        val adapter = Datadapter(this, dataList, true)
+
+        recyclerView.setHasFixedSize(true)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = adapter
+
+        override fun onDestroy() {
+            super.onDestroy()
+            realm.close()
+        }
+
+        fun createDummyData() {
+            for (i in 0..10) {
+                create( "投稿 $i")
+            }
+        }
+
+        fun create(imageId: Int, content: String) {
+            realm.executeTransaction {
+                val task = it.createObject(Data::class.java, UUID.randomUUID().toString())
+                task.imageId = imageId
+                task.content = content
+            }
+        }
+
+        fun readAll(): RealmResults<Data> {
+            return realm.where(Data::class.java).findAll().sort("createdAt", Sort.ASCENDING)
+        }
 
         //投稿ボタンを押した時に画面が遷移する
         tokoButton.setOnClickListener {
--- app/src/test/java/app/wataso_/watanabe/omoshirorecycle/DataAdapter.kt
+++ app/src/test/java/app/wataso_/watanabe/omoshirorecycle/DataAdapter.kt
@@ -8,38 +8,39 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import io.realm.OrderedRealmCollection
 import io.realm.RealmRecyclerViewAdapter
+import kotlinx.android.synthetic.main.item_data_cell.view.*
 import org.junit.runner.manipulation.Ordering
 import java.text.SimpleDateFormat
 import java.util.*
 
 class DataAdapter(
     private val context: Ordering.Context,
-    private var taskList: OrderedRealmCollection<Data>?,
+    private var dataList: OrderedRealmCollection<Data>?,
     private val autoUpdate: Boolean
 ) :
-    RealmRecyclerViewAdapter<Data, DataAdapter.TaskViewHolder>(taskList, autoUpdate) {
+    RealmRecyclerViewAdapter<Data, DataAdapter.TaskViewHolder>(dataList, autoUpdate) {
 
-    override fun getItemCount(): Int = taskList?.size ?: 0
+    override fun getItemCount(): Int = dataList?.size ?: 0
 
     override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
-        val task: Data = taskList?.get(position) ?: return
+        val data: Data = dataList?.get(position) ?: return
 
-        holder.imageView.setImageResource(task.imageId)
-        holder.contentTextView.text = task.content
+        holder.genreTextView.text = data.content
+        holder.contentTextView.text = data.content
         holder.dateTextView.text =
-            SimpleDateFormat("yyyy/MM/dd HH:mm:ss", Locale.JAPANESE).format(task.createdAt)
+            SimpleDateFormat("yyyy/MM/dd HH:mm:ss", Locale.JAPANESE).format(data.createdAt)
 
     }
 
     override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): TaskViewHolder {
-        val v = LayoutInflater.from(context).inflate(R.layout.list_item, viewGroup, false)
+        val v = LayoutInflater.from(context).inflate(R.layout.item_data_cell, viewGroup, false)
         return TaskViewHolder(v)
     }
 
     class TaskViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val imageView: ImageView = view.imageView
+        val genreTextView: TextView = view.genreTextView
         val contentTextView: TextView = view.contentTextView
         val dateTextView: TextView = view.dateTextView
-    }
+    }sss
 
 }
